eea97cdebdf12bc7d851062e5964b4124232d6fc
shantel2 <shantelwellington03@gmail.com>
2022-04-01 11:32:41 -0700

    search bar works

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script>
-  import NewsList from '@/components/NewsList.vue'
+  //import NewsList from '@/components/NewsList.vue'
     export default {
     data() {
       return {
         message: "Hello World! Welcome to VueJS"
       };
     },
-  components: { NewsList }
+  //components: { NewsList }
 }
 </script>
 <template>
@@ -14,7 +14,7 @@
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
-        <NewsList> </NewsList>
+        <!-- <NewsList> </NewsList> -->
       </div>
     </div>
 </template>
---
+++ src/views/NewsView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h2>News</h2>
+        <NewsList> </NewsList>
+    </div>
+</template>
+
+<script>
+import NewsList from '@/components/NewsList.vue'
+    export default {
+        data() {
+            return {};
+        },
+        components:{NewsList}
+};
+</script>
+
+
+<style>
+
+</style>
