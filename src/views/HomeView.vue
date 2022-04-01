eb751c21c370e6eecbf2aaea5f84804bec437b9f
shantel2 <shantelwellington03@gmail.com>
2022-04-01 10:27:58 -0700

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <script>
-export default {
+  import NewsList from '@/components/NewsList.vue'
+    export default {
     data() {
-        return {
-            message: "Hello World! Welcome to VueJS"
-        };
+      return {
+        message: "Hello World! Welcome to VueJS"
+      };
     },
+  components: { NewsList }
 }
 </script>
-
 <template>
     <div class="container">
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
+        <NewsList> </NewsList>
       </div>
     </div>
 </template>
